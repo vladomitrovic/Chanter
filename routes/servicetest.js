0a89a0c91c976984c010d8290cc1d42e4f31365b
Codemanja <codemanjaa@gmail.com>
2018-10-08 08:45:31 +0200

    Service activated.

---
+++ routes/servicetest.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('service/index', { title: 'Express' });
+});
+
+router.get('/petites', function(req, res, next) {
+    res.render('service/petites', { title: 'Express' });
+});
+
+
+router.get('/forumpres', function (req, res, next) {
+    res.render('service/forumpres', {title: 'Express'});
+
+});
+
+
+router.get('/ascj', function (req, res, next) {
+    res.render('service/ascj', {title: 'Express'});
+});
+
+router.get('/newsletter', function (req, res, next) {
+    res.render('service/newsletter', {title: 'Express'});
+});
+
+
+router.get('/servicedep', function (req, res, next) {
+    res.render('service/servicedep', {title: 'Express'});
+});
+
+router.get('/contact', function (req, res, next) {
+    res.render('service/contacts', {title: 'Express'});
+});
+module.exports = router;
