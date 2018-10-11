18608e227bac8e3f458aaa1f0595791f46e66af9
celahm <34541727+celahm@users.noreply.github.com>
2018-10-11 10:18:06 +0200

    Page login routes ok

--- routes/presentation.js
+++ routes/presentation.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('presentation/index', { title: {langs:presentation} });
+    var langs = require('../lang/'+ res.locals.langUsed  );
+    res.render('presentation/index', { title:langs.presentation });
 });
 
 router.get('/comite', function(req, res, next) {
-    res.render('presentation/comite',{ title: {langs:comite} });
+    var langs = require('../lang/'+ res.locals.langUsed  );
+    res.render('presentation/comite',{ title: langs.comite });
 });
 
 router.get('/commission', function(req, res, next) {
-    res.render('presentation/commission',{ title: {langs:commission} });
+    res.render('presentation/commission',{ title: langs.commission });
 });
 
 router.get('/groupement', function(req, res, next) {
-    res.render('presentation/groupement',{ title: {langs:groupements}});
+    res.render('presentation/groupement',{ title: langs.groupements});
 });
 
 router.get('/honneur', function(req, res, next) {
-    res.render('presentation/honneur',{ title: {langs:honneur}});
+    res.render('presentation/honneur',{ title: langs.honneur});
 });
 
 module.exports = router;
