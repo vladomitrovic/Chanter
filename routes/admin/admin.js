7721dd53a1ae287e56e1900f6a648c3cf8c608f0
vlado <vlado.mitrovic@outlook.com>
2018-10-22 21:27:52 +0200

    Add articles in DB OK, change pwd en cours

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,10 +2,41 @@ var express = require('express');
 var router = express.Router();
 var articleRouter = require('./articles')
 var choeursRouter = require('./choeurs')
+var models = require('../../models');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('admin/index', { title: 'Admin',  layout:'admin/adminLayout'});
+    res.render('admin/index', { title: 'Admin', user: req.session.user,  layout:'admin/adminLayout'});
+});
+
+router.get('/params', function(req, res, next) {
+    res.render('admin/params', {user: req.session.user,  layout:'admin/adminLayout'});
+});
+
+router.post('/params/pwd', function(req, res, next) {
+
+    if(req.body.newPwd!=req.body.newPwd2){
+        req.flash('error', 'pwd !=');
+        res.redirect('./');
+    }
+
+    var user = req.session.user;
+
+    bcrypt.compare(req.body.actualPwd, user.password_hash).then((correct) => {
+        if (correct === true) {
+            models.Person.update({password: req.body.newPwd},
+            {where: user.id})
+        }
+        else {
+            req.flash('error', 'Password incorrect!');
+            res.redirect('./');
+        }
+    }).catch(console.error);
+
+
+    res.render('admin/params', {user: req.session.user,  layout:'admin/adminLayout'});
 });
 
 router.use('/articles', articleRouter);
--- routes/admin/choeurs.js
+++ routes/admin/choeurs.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // New chore form
 router.get('/new', function(req, res, next) {
 
-    res.render('admin/registration', {layout:'admin/adminLayout'});
+    res.render('admin/registration', {layout:'admin/adminLayout', user: req.session.user,});
 });
 
 
