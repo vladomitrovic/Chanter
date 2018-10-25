e9ed8a8f03bc4fed37db6c790b43f335fae11408
jonHESSO <jonathan.schnyder@students.hevs.ch>
2018-10-25 12:46:17 +0200

    started admin edition of choirs

--- routes/admin/choeurs.js
+++ routes/admin/choeurs.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../../models')
 
 
 // New chore form
 router.get('/new', function(req, res, next) {
 
-    res.render('admin/registration', {layout:'admin/adminLayout', user: req.session.user,});
+    res.render('admin/editChoir', {layout:'admin/adminLayout', user: req.session.user, choeur: new models.Choir(), action: "/new"});
+});
+
+router.get('/update/:id', function(req, res, next) {
+    models.Choir.findOne({where:{id: req.params.id}, include: models.Locality}).then((choir) => {
+        console.log(JSON.stringify(choir));
+        res.render('admin/editChoir', {layout:'admin/adminLayout', user: req.session.user, choeur: choir, action: "/edit"});
+    });
+});
+
+router.post('/update/:id', function(req, res, next) {
+
+});
+
+router.get('/delete', function(req, res, next) {
+
+    res.render('admin/deleteChoir', {layout:'admin/adminLayout', user: req.session.user,});
 });
 
 
