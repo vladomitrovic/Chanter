11a7b1ea93151726c2ae55b216e383b1f16226ea
vlado <vlado.mitrovic@outlook.com>
2018-10-25 14:25:05 +0200

    List articles, delete, update(not finish)

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -11,21 +11,68 @@ router.get('/', function(req, res, next) {
 });
 
 
-// New article from
-router.get('/new', function(req, res, next) {
-
-    models.Category.findAll().then((categories) => {
-
-            res.render('admin/newArticle', {category:categories , user: req.session.user,  title: 'Ajouter un article',  layout:'admin/adminLayout'});
-
+// List articles
+router.get('/list', function(req, res, next) {
+    models.Article.findAll().then((articles) => {
+        res.render('admin/listArticles', {articles:articles, user: req.session.user, layout:'admin/adminLayout'});
     });
 });
 
-// Add the article || TO IMPLEMENT !!!
+
+// New article from
+router.get('/new', function(req, res, next) {
+    models.Category.findAll().then((categories) => {
+            res.render('admin/newArticle', {category:categories , user: req.session.user, layout:'admin/adminLayout', action:'add'});
+    });
+});
+
+router.get('/modify/:id', function(req, res, next) {
+
+    models.Article.findOne({
+        where : {id:  req.params.id}
+    }).then((article) => {
+         models.Category.findAll().then((categories) => {
+                res.render('admin/newArticle', {category:categories , user: req.session.user, layout:'admin/adminLayout',article: article , action:'modify'});
+        });
+    });
+});
+
+
+router.get('/delete/:id',  (req, res) => {
+    models.Article.destroy({
+        where : {id:  req.params.id}
+    }).then(() => {
+        res.redirect('/admin/articles/list')
+    });
+});
+
+// Add the article
 router.post('/add', function(req, res, next) {
+    models.Category.findOne({
+        where: {
+            [Op.or]: [{categoryFR: req.body.categorie}, {categoryDE: req.body.categorie}]
+        }
+    }).then((category) => {
+        var categoryId = category.id;
+        var user = req.session.user;
 
+        models.Article.create({
+            titleFR: req.body.titleFR,
+            chapeauFR: req.body.chapeauFR,
+            textFR: req.body.textFR,
+            titleDE: req.body.titleDE,
+            chapeauDE: req.body.chapeauDE,
+            textDE: req.body.textDE,
+            refPicture: req.body.myFile,
+            PersonId :user.id,
+            CategoryId: categoryId
+        }).then(() => {
+            res.redirect('./new');
+        })
+    }).catch(console.error);
+});
 
-
+router.post('/modify', function(req, res, next) {
     models.Category.findOne({
         where: {
             [Op.or]: [{categoryFR: req.body.categorie}, {categoryDE: req.body.categorie}]
@@ -48,13 +95,9 @@ router.post('/add', function(req, res, next) {
         }).then(() => {
             res.redirect('./new');
         })
-
     }).catch(console.error);
-
-
-
-
 });
+
 
 
 module.exports = router;
