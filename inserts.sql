7baf00ad65905f66a4b546725644f5383aaafd90
celahm <34541727+celahm@users.noreply.github.com>
2018-10-29 09:24:36 +0100

    script

--- inserts.sql
+++ inserts.sql
@@ -5,8 +5,28 @@ A la suite d’un week-end de l’AVDC (Association Vaudoise des Directeurs de C
 '<h3>Zielsetzung</h3>Um den Chorleitern die Möglichkeit zu geben, ihre Ausbildung mit kompetenten Dozenten zu perfektionieren oder Anliegen und Freuden mit Kollegen zu teilen, setzt das AVCC seine anfängliche Tätigkeit mit seinen Förderungen und Dienstleistungen fort.<h3>Fundament</h3>Nach einem Wochenende der AVDC (Association Vaudoise des Directeurs de Chœurs) in Vérossaz beschlossen rund zehn walliserische Chordirigenten, die AVCC, die Association Wallisanne des Chefs de Chœur zu gründen. Diese Stiftung fand am 11. Oktober 1969 um Herrn Fernand Dubois, den verstorbenen Präsidenten des Kantonalen Gesangsverbandes, statt. Die Gründungsversammlung fand am 13. Dezember 1969 in Sion statt, in Anwesenheit von etwa dreißig Leitern, die den Aufruf entgegennahmen.<h3>Präsidenten</h3>Unter dem Vorsitz von Armand Blanc, Jean-Pierre Salamin, Pierre-Louis Nanchen, Pierre-Alain Barras, Samuel Emery und seit 2014 Jean-David Waeber hat die AVCC die von den Initiatoren und Gründern festgelegten Ziele ständig verfolgt und an den Geschmack und die Bedürfnisse des Augenblicks angepasst.<h3>Foyer</h3>Das Foyer Musical, das vom AVCC am 28. August 1979 im Maison Supersaxo in Martigny eröffnet wurde, wurde 1981 nach Sion verlegt, in das Foyer Don Bosco, dann bis 2009 in das Foyer de Mazerette. In diesem Jahr vermachte das AVCC seine Partiturensammlung der Médiathèque Wallis, um ihr mehr Sichtbarkeit in der Öffentlichkeit zu verschaffen. Diese Sammlung umfasst seit Jahren fast 20.000 Partituren und zahlreiche Sammlungen, Sammlungen und Tonträger, die alle von Edouard Delaloye, dem ersten Animateur, und dann von Anne-Françoise Andenmatten-Sierro, der derzeitigen Archivarin des Vereins, die seit 1985 tätig ist, aufgeführt werden.',
 'article1.jpg', '2018-10-24 00:00:00', '2018-10-24 00:00:00', 1, 1),
 
-(4, 'Présentation AVCC', 'AVCC-Presentation', 'chapeau fr', 'chapeau de', '
-'
+(4, 'Présentation AVCC', 'AVCC-Presentation', 'chapeau fr', 'chapeau de', '<p>L&rsquo;Association Valaisanne des Chefs de Choeurs (AVCC) est une association qui, depuis plus de 40 ans, veut promouvoir la formation continue des chefs de chœur grâce à des cours donnés par des intervenants réputés et de qualité. Elle tient à défendre les intérêts des chefs de chœurs, à promouvoir une formation continue de qualité, à favoriser les contacts entre ses sociétaires et les organes officiels, à offrir à ses membres une documentation spécialisée dans le domaine du chant choral.</p>
+<p>Elle organise chaque année, seule ou en collaboration avec la Fédération des Sociétés de Chant du Valais, des séminaires avec des chefs renommés :</p>
+<ul>
+<li>des journées de formation</li>
+<li>des soirées à thème</li>
+<li>des journées spéciales pour chefs de chœurs de jeunes ou d’enfants</li>
+<li>des activités autour du fonds choral déposé auprès de la Médiathèque Valais</li>
+</ul>
+<p>Elle favorise également la rencontre et les échanges entre chefs et propose un vaste choix de plus de 20’000 partitions.</p>
+<p>Merci de nous aider à faire connaître l’AVCC ! N’hésitez pas à nous communiquer vos souhaits, vos soucis, vos coups de cœur ou vos questions. Nous essayons de tout mettre en œuvre pour répondre à vos besoins.</p>
+<p>N&rsquo;hésitez pas à découvrir :</p>
+<ul>
+<li><a title="notre historique AVCC" href="http://www.chanter.ch/wp/avcc/historique/">l&rsquo;historique AVCC</a></li>
+<li><a title="Comité AVCC" href="http://www.chanter.ch/wp/avcc/comite-avcc/">notre comité</a></li>
+<li><a title="Formation AVCC" href="http://www.chanter.ch/wp/avcc/formation-avcc/">notre offre de formation</a></li>
+<li><a title="Fonds choral et Foyer AVCC" href="http://www.chanter.ch/wp/avcc/fonds-choral-avcc/">notre fonds de partitions</a></li>
+<li><a title="Photos AVCC" href="http://www.chanter.ch/wp/avcc/photos-avcc-3/">les photos de nos activités passées</a></li>
+</ul>
+
+
+
+',
 'Die Vereinigung Wallis des Chefs de Chœur (AVCC) ist eine Vereinigung, die seit mehr als 40 Jahren die Weiterbildung von Chordirigenten durch Kurse von renommierten und qualifizierten Lehrern fördern will. Sie setzt sich für die Interessen der Chorleiter ein, fördert eine qualitativ hochwertige Weiterbildung, pflegt die Kontakte zwischen ihren Mitgliedern und den offiziellen Stellen und bietet ihren Mitgliedern eine spezielle Dokumentation im Bereich des Chorgesangs.
 
 Jedes Jahr organisiert sie allein oder in Zusammenarbeit mit der Fédération des Sociétés de Chant du Valais Seminare mit renommierten Köchen:
@@ -123,11 +143,29 @@ Der FSCV wird von Laurent Bovier mit Kompetenz und Begeisterung geleitet. Er bes
 'article1.jpg', '2018-10-24 00:00:00', '2018-10-24 00:00:00', 1, 1);
 
 
-INSERT INTO `People` (`id`, `lastname`, `firstname`, `birthdate`, `address1`, `adress2`, `phone`, `phoneprof`, `fax`, `email`, `mobile`, `lastupdate`, `username`, `password_hash`, `createdAt`, `updatedAt`, `GenderId`, `LanguageId`, `LocalityId`) VALUES
-(3, 'Bovier', 'Laurent ', '1991-12-12', 'Rue de la rue 23', NULL, '0233216546', '1234567890', '1234567890', 'john@doe.com', '1234567890', '2018-10-02 00:00:00', 'j', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-03 00:00:00', '2018-10-03 00:00:00', 1, 1, 1),
-(4, 'Dayer', 'Valentin ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'use', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3),
-(5, 'Lovisa', 'Blaise ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'usr1', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3),
-(6, 'Meyer', 'Nadine ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'er1', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3);
+(8, 'Présentation', 'Presentation', 'chapeau fr', 'chapeau de',
+'Cette journée animée par Charles Barbier se veut interactive et propose de réfléchir aux aspects cachés de notre pratique de chefs de chœur, notamment la communication non verbale et les choix que nous faisons avant même d’être face au chœur. En choisissant comme fil rouge la prise de risque, nous aurons l’occasion de partager des regards neufs sur nos pratiques. Quels risques prend-on en élaborant un programme de concert ? Sans faire de mise en voix fait-on courir un risque aux chanteurs ? Y a-t-il un risque à ne pas prêter attention au choix de ses mots, en répétition ?
+
+Cours gratuit pour les membres cotisants AVCC, 40.- CHF pour les non cotisants à l’AVCC / Repas : 25.- CHF / Encaissement sur place.
+
+Inscriptions jusqu’au 10 novembre 2018 à associationavcc@gmail.com ou au 079 930 25 43. Merci d’indiquer si vous souhaitez partager le repas avec les autres participants.',
+'Dieser Tag unter der Leitung von Charles Barbier soll interaktiv sein und schlägt vor, über die verborgenen Aspekte unserer Praxis als Chorleiter nachzudenken, insbesondere die nonverbale Kommunikation und die Entscheidungen, die wir treffen, bevor wir überhaupt dem Chor gegenüber stehen. Wenn wir uns für die Risikobereitschaft als gemeinsamen Nenner entscheiden, haben wir die Möglichkeit, neue Perspektiven auf unsere Praktiken auszutauschen. Welche Risiken werden bei der Entwicklung eines Konzertprogramms eingegangen? Ohne Stimme, setzen wir die Sänger in Gefahr? Besteht das Risiko, dass man bei der Probe nicht auf die Wortwahl achtet?
+
+Kostenloser Kurs für AVCC-Mitglieder, CHF 40 für nicht AVCC-Mitglieder / Mahlzeiten: CHF 25 / Abholung vor Ort.
+
+Anmeldungen bis zum 10. November 2018 unter associationavcc@gmail.com oder 079 930 25 43. Bitte geben Sie an, ob Sie das Essen mit den anderen Teilnehmern teilen möchten.
+r',
+'article1.jpg', '2018-10-24 00:00:00', '2018-10-24 00:00:00', 1, 2);
+
+
+
+
+
+INSERT INTO `People` (`id`, `lastname`, `firstname`, `birthdate`, `address1`, `adress2`, `phone`, `phoneprof`, `fax`, `email`, `mobile`, `lastupdate`, `username`, `password_hash`, `createdAt`, `updatedAt`, `GenderId`, `LanguageId`, `LocalityId`,, `FunctionId`) VALUES
+(3, 'Bovier', 'Laurent ', '1991-12-12', 'Rue de la rue 23', NULL, '0233216546', '1234567890', '1234567890', 'john@doe.com', '1234567890', '2018-10-02 00:00:00', 'j', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-03 00:00:00', '2018-10-03 00:00:00', 1, 1, 1,1),
+(4, 'Dayer', 'Valentin ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'use', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3,1),
+(5, 'Lovisa', 'Blaise ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'usr1', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3,1),
+(6, 'Meyer', 'Nadine ', '1989-01-01', 'Rue de la dixence 13', NULL, '', NULL, NULL, 'donotcont@me', '0777777777', NULL, 'er1', '$2a$10$PpSWF8s1l/qf1HDrmQsj/.aerK96nCfNO3ynjxX88MIXy1LnZxANq', '2018-10-07 00:00:00', '2018-10-07 00:00:00', 3, 2, 3,1);
 
 
 INSERT INTO `Comity_Person` (`createdAt`, `updatedAt`, `ComityId`, `PersonId`) VALUES
