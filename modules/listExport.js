e73fc930a4787f0da9bc6fe5d40585e34128014f
vlado <vlado.mitrovic@outlook.com>
2018-10-30 15:41:50 +0100

    Export choirs

--- modules/listExport.js
+++ modules/listExport.js
@@ -3,12 +3,16 @@ var models = require('../models');
 
 // create workbook & add worksheet
 var workbook = new Excel.Workbook();
-var worksheet = workbook.addWorksheet('Person');
+var worksheet = workbook.addWorksheet('List');
 
 var self = module.exports = {
 
     getListUser() {
         return new Promise((resolve, reject) => {
+
+            worksheet.destroy();
+            worksheet = workbook.addWorksheet('ListPerson');
+
         // add column headers
         worksheet.columns = [
             {header: 'Lastname', key: 'lastname'},
@@ -42,5 +46,58 @@ var self = module.exports = {
 
 
         });
+    },
+
+    getListChoir() {
+        return new Promise((resolve, reject) => {
+            worksheet.destroy();
+            worksheet = workbook.addWorksheet('ListChoir');
+
+            // add column headers
+            worksheet.columns = [
+                {header: 'choirName', key: 'choirName'},
+                {header: 'address1', key: 'address1'},
+                {header: 'address2', key: 'address2'},
+                {header: 'website', key: 'website'},
+                {header: 'otherType', key: 'otherType'},
+                {header: 'yearOfCreation', key: 'yearOfCreation'},
+                {header: 'chEglise', key: 'chEglise'},
+                {header: 'chGospel', key: 'chGospel'},
+                {header: 'entryfc', key: 'entryfc'},
+                {header: 'entryGroup', key: 'entryGroup'},
+                {header: 'remarks', key: 'remarks'}
+
+            ];
+
+            models.Choir.findAll().then((choirs) => {
+
+                choirs.forEach(function (choir) {
+                    worksheet.addRow({
+                        choirName: choir.choirName,
+                        address1: choir.address1,
+                        adress2: choir.adress2,
+                        website: choir.website,
+                        otherType: choir.otherType,
+                        yearOfCreation: choir.yearOfCreation,
+                        chEglise: choir.chEglise,
+                        chGospel: choir.chGospel,
+                        entryfc: choir.entryfc,
+                        entryGroup: choir.entryGroup,
+                        remarks: choir.remarks
+                    });
+
+                    workbook.xlsx.writeFile('list.xlsx').then(function () {
+                        resolve();
+                    });
+                })
+            }).catch((error) => {
+                reject(error);
+            })
+
+
+        });
     }
+
+
+
 };
--- routes/admin/liste.js
+++ routes/admin/liste.js
@@ -3,22 +3,30 @@ var listExport = require('../../modules/listExport')
 var router = express.Router();
 
 
-// New chore form
-router.get('/person', function(req, res, next) {
+
+
+
+router.get('/people', function(req, res, next) {
     listExport.getListUser().then(() => {
 
-
-        //Now I want to download that file
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         res.setHeader('Content-Disposition', 'attachment; filename=list.xlsx');
         res.download('list.xlsx', 'list.xlsx');
 
-    console.log("start dwnld");
-    });
 
+
+    });
 });
 
 
+router.get('/choirs', function(req, res, next) {
+    listExport.getListChoir().then(() => {
+
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', 'attachment; filename=list.xlsx');
+        res.download('list.xlsx', 'list.xlsx');
+    });
+});
 
 
 module.exports = router;
